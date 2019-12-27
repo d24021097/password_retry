514083ee994ab14b9f962af89c4a2d3a97f4e1e8
JulieH <d24021097@gmail.com>
2019-12-27 15:59:37 +0800

    add

--- password_retry.py
+++ password_retry.py
@@ -1,13 +1,14 @@
 password = 'a123456'
 i = 3 #剩下三次機會
-while True:
+while i > 0:
+	i = i - 1
 	pwd = input('請輸入密碼:') #要用新變數pwd
 	if pwd == password: #最好輸入password而非a123456
 		print('登入成功!')
 		break #逃出迴圈
 	else:
-		i = i - 1
-		print('密碼錯誤！還有', i, '次機會')
-		if i == 0:
-			print('超過登入次數3次，請過十分鐘重試')
-			break
+		print('密碼錯誤！')
+		if i > 0:
+			print('還有', i, '次機會')
+		else:
+			print('密碼錯誤三次,帳號被鎖住')
